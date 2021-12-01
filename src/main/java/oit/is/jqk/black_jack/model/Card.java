1ac15995afb42c0f4f58bfe49bbc9706032d55ff
e1b19020 <e1b19020@oit.ac.jp>
2021-12-02 00:22:05 +0900

    ディーラーのヒットとスタンドの実装

--- src/main/java/oit/is/jqk/black_jack/model/Card.java
+++ src/main/java/oit/is/jqk/black_jack/model/Card.java
@@ -1,30 +1,31 @@
 package oit.is.jqk.black_jack.model;
-public class Card {
-    int id;
-    String suit;
-    int number;
 
-    public String getSuit() {
-        return suit;
-      }
-    
-      public void setSuit(String suit) {
-        this.suit = suit;
-      }
-    
-      public int getNumber() {
-        return number;
-      }
-    
-      public void setNumber(int number) {
-        this.number = number;
-      }
-    
-      public int getId() {
-        return id;
-      }
-    
-      public void setId(int id) {
-        this.id = id;
-      }
+public class Card {
+  int id;
+  String suit;
+  int number;
+
+  public String getSuit() {
+    return suit;
+  }
+
+  public void setSuit(String suit) {
+    this.suit = suit;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
